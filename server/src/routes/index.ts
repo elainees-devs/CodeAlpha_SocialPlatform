a44ffa48d417298fd1bbbcefe531fb4999dfa4a7
elainees-devs <elainees.devs@gmail.com>
2026-04-15 16:09:35 +0300

    chore(routes): add central route exports (barrel file)

---
+++ server/src/routes/index.ts
@@ -0,0 +1,7 @@
+export { default as authRoutes } from "./auth.routes";
+export { default as userRoutes } from "./users.route";
+export { default as postRoutes } from "./posts.routes";
+export { default as commentRoutes } from "./comments.routes";
+export { default as likeRoutes } from "./likes.routes";
+export { default as followRoutes } from "./follows.routes";
+
