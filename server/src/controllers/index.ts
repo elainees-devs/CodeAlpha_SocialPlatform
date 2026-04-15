f9967f4393ac8f122844bcd74c85b3ad9e28062f
elainees-devs <elainees.devs@gmail.com>
2026-04-15 15:51:20 +0300

    chore(controllers): add central export barrel for all controllers

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,6 @@
+export { userController } from "./users.controllers";
+export { authController } from "./auth.controllers";
+export { postController } from "./posts.controllers";
+export { commentController } from "./comments.controllers";
+export { likeController } from "./likes.controllers";
+export { followController } from "./follows.controllers";
