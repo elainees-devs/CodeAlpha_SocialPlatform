00e1b0d595497e70d8d761281d7f6002793715d1
elainees-devs <elainees.devs@gmail.com>
2026-04-20 14:28:33 +0300

    chore: setup frontend project structure and install dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Welcome to Social Connect platform!</h1>
+      <p>This is a simple React application using TypeScript.</p>
+    </>
+  )
+}
+
+export default App
