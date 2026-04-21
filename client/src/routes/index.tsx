7eecc3011da94a31338ca4ef788e1890cb809f79
elainees-devs <elainees.devs@gmail.com>
2026-04-21 21:00:48 +0300

    refactor(router): restructure routes with nested layout and profile route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,6 +1,6 @@
 // src/routes/index.tsx
 import { createBrowserRouter } from "react-router-dom";
-import { Home, Login, Register } from "../pages";
+import { Home, Login, Profile, Register } from "../pages";
 import Feed from "../pages/Feed";
 import AuthGate from "../components/auth/AuthGate";
 import ProtectedRoute from "./ProtectedRoute";
@@ -10,30 +10,19 @@ export const router = createBrowserRouter([
   {
     element: <AuthGate />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
+      { path: "/", element: <Home /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
 
-      //Protected layout section
       {
-        element: (
-          <ProtectedRoute>
-            <AppLayout />
-          </ProtectedRoute>
-        ),
+        element: <ProtectedRoute />, 
         children: [
           {
-            path: "/feed",
-            element: <Feed />,
+            element: <AppLayout />,  
+            children: [
+              { path: "/feed", element: <Feed /> },
+              { path: "/profile", element: <Profile /> }
+            ],
           },
         ],
       },
