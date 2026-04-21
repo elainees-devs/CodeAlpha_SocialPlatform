f796ed03cbbcf12d7842fd5e8ec91f14cb5acd20
elainees-devs <elainees.devs@gmail.com>
2026-04-21 20:40:20 +0300

    feat(store): implement auth state management with Zustand persistence

---
+++ client/src/store/auth.store.ts
@@ -0,0 +1,51 @@
+// src/store/auth.store.ts
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import type { User } from "../types/user.types";
+
+type AuthState = {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  hydrated: boolean;
+
+  login: (user: User, token: string) => void;
+  logout: () => void;
+  setHydrated: () => void;
+};
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+      hydrated: false,
+
+      login: (user, token) =>
+        set({
+          user,
+          token,
+          isAuthenticated: true,
+        }),
+
+      logout: () =>
+        set({
+          user: null,
+          token: null,
+          isAuthenticated: false,
+        }),
+
+      setHydrated: () =>
+        set({ hydrated: true }),
+    }),
+    {
+      name: "auth-storage",
+
+    
+      onRehydrateStorage: () => (state) => {
+        state?.setHydrated();
+      },
+    }
+  )
+);
