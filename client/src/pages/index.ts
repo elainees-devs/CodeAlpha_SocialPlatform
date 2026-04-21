2b9b7e5116f7a629a78b70844d9d317e005d7bcf
elainees-devs <elainees.devs@gmail.com>
2026-04-21 20:34:18 +0300

    chore: add and reorganize page exports for routing

---
+++ client/src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as NotFound } from "./NotFound";
+export { default as Register } from "./auth/Register";
+export { default as Home } from "./Home";
+export { default as Login } from "./auth/Login";
+export { default as Feed } from "./Feed";
+export { default as Profile } from "./Profile";
