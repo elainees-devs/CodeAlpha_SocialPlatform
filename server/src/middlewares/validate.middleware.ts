4df6dbececcaf01c73920cfd377e507bccdceb88
elainees-devs <elainees.devs@gmail.com>
2026-04-22 18:52:25 +0300

    refactor(validation): improve error handling and forward Zod errors to ApiError

--- server/src/middlewares/validate.middleware.ts
+++ server/src/middlewares/validate.middleware.ts
@@ -1,16 +1,17 @@
+// src/middlewares/validate.middleware.ts
 import { Request, Response, NextFunction } from "express";
 import { ZodSchema } from "zod";
 import { ApiError } from "../utils";
 
 export const validate =
   <T>(schema: ZodSchema<T>) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  (req: Request, _res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
 
     if (!result.success) {
-      const errors = result.error.flatten();
-
-      throw new ApiError(400, "Validation failed");
+      return next(
+        new ApiError(400, "Validation failed", result.error.flatten())
+      );
     }
 
     req.body = result.data;
