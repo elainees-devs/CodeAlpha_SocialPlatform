d47d42138a609d1269145448774a296523d2e8a4
elainees-devs <elainees.devs@gmail.com>
2026-04-22 19:00:08 +0300

    chore(services): add barrel exports for service modules

---
+++ server/src/services/index.ts
@@ -0,0 +1,7 @@
+// src/services/index.ts
+export { authService } from "./auth.service";
+export {userService} from "./users.service";
+export {postService} from "./posts.service";
+export {commentService} from "./comments.service";
+export {likeService} from "./likes.service";
+export {followService} from "./follows.service";
