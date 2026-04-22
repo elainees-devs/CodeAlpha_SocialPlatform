717fa162eab20508e9bae1d4a862efb78f85a6ff
elainees-devs <elainees.devs@gmail.com>
2026-04-22 19:16:03 +0300

    chore: remove unused files

--- server/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { userModel } from './users.models';
-export { postModel } from './posts.models';
-export {commentModel} from './comments.models';
-export { likeModel } from './likes.models';
-export { followModel } from './follows.models';
