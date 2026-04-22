94e8fc8f9b451aff1da281c9200b93fa3e088d1f
elainees-devs <elainees.devs@gmail.com>
2026-04-22 19:11:03 +0300

    refactor(utils): enhance ApiError with error payload support

--- server/src/utils/app-error.ts
+++ server/src/utils/app-error.ts
@@ -1,11 +1,14 @@
+// src/utils/app-error.ts
 export class ApiError extends Error {
   public statusCode: number;
   public isOperational: boolean;
+  public errors?: unknown;
 
-  constructor(statusCode: number, message: string) {
+  constructor(statusCode: number, message: string, errors?: unknown) {
     super(message);
 
     this.statusCode = statusCode;
+    this.errors = errors;
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
