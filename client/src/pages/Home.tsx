fbf2778b5223ee01ee9392f72cdd95e4d51dfc4b
elainees-devs <elainees.devs@gmail.com>
2026-04-23 22:57:05 +0300

    feat(home): add navigation link to login page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -19,6 +19,12 @@ export default function Home() {
       >
         Go to Register
       </Link>
+      <Link
+        to="/login"
+        className="mt-4 bg-gray-600 hover:bg-gray-700 text-white px-4 py-2 rounded"
+      >
+        Go to Login
+      </Link>
 
     </div>
   );
